2dd0d4c67d40c56b1be04b092bf945890cc7bece
IgYaHiko <subhrokolay2@gmail.com>
2026-08-10 08:11:04 +0530

    blah blah

---
+++ rev.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace  std;
+class Solution {
+public:
+        vector<char> rev(vector<char>& s) {
+            int i =0;
+            int j=s.size()-1;
+            while (i<j)
+            {
+                char temp=s[i];
+                s[i]=s[j];
+                s[j]=temp;
+                i++;
+                j--;
+            }
+            return s;
+        }
+};
+int main() {
+    vector<char> s = {'h','e','l','l','o'};
+    Solution sol;
+    vector<char> ans = sol.rev(s);
+    
+    for (char x: ans) {
+         cout << x << " ";
+
+    }
+    cout << endl;
+    return 0;
+}
