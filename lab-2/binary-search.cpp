d395bcd674827eaeb3fbd8f986c8c27627889567
IgYaHiko <subhrokolay2@gmail.com>
2026-08-11 14:44:18 +0530

    [PATCH]-[04]:lab-2-1st-qs

---
+++ lab-2/binary-search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution{
+public:
+    int binarySearch(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size()-1;
+        
+        while (i<=j) {
+            int mid = (i+j)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (target < nums[mid]) {
+                j = mid - 1;
+                if (target == nums[mid]) {
+                    return mid;
+                }
+            } else {
+                i = mid + 1;
+                if(target == nums[mid]) {
+                  return mid;
+                }
+            }
+
+        }
+
+         return -1;
+    }
+};
+
+int main() {
+    cout << "Enter your Target" << endl;
+    int target;
+    
+    cin >> target;
+    vector<int> nums = {1,2,3,4,5};
+    Solution sol;
+    cout << sol.binarySearch(nums,target) << endl;
+}
