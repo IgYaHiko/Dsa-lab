ffd8fdae971c7354ab480c832d72a100dbabc379
IgYaHiko <subhrokolay2@gmail.com>
2026-09-12 17:01:26 +0530

    practice-sorting

---
+++ sorting/bubble.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> bubblesort(vector<int> &nums) {
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=0; j<nums.size()-i-1; j++) {
+                if(nums[j] > nums[j+1]) {
+                    swap(nums[j], nums[j+1]);
+                }
+            }
+        }
+    return nums;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4,3,52,3,0,3,1};
+    vector<int> ans = sol.bubblesort(nums);
+    cout << "sorted Arrray: " << endl;
+    for(int x: ans) {
+        cout << x << " ";
+    }
+    return 0;
+}
